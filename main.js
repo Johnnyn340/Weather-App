70476506111d1cea92d905b4ff7d7883955ae54e
Johnnyn340 <114721267+Johnnyn340@users.noreply.github.com>
2023-03-23 23:38:13 -0700

    Added error handling and more content

--- main.js
+++ main.js
@@ -4,17 +4,31 @@ const degree = document.querySelector(".degree");
 const key = "fc2b279a4d6c496eb3600946232203";
 const searchBox = document.querySelector(".search input");
 const api = "http://api.weatherapi.com/v1/current.json?key=";
+const error = document.querySelector(".error");
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
+const feels = document.querySelector(".feels-like");
+
 async function getWeather(city) {
   const response = await fetch(
     api + key + `&q=${city}"`,
 
     { mode: "cors" }
   );
-  const weather = await response.json();
-  place.innerHTML = weather.location.name;
-  condition.innerHTML = weather.current.condition.text;
-  degree.innerHTML = weather.current.temp_f;
-  console.log(weather);
+
+  if (response.status == 400) {
+    error.style.display = "block";
+  } else {
+    const weather = await response.json();
+    place.innerHTML = weather.location.name;
+    condition.innerHTML = weather.current.condition.text;
+    degree.innerHTML = weather.current.temp_f;
+    wind.innerHTML = weather.current.wind_mph;
+    humidity.innerHTML = weather.current.humidity;
+    feels.innerHTML = weather.current.feelslike_f;
+    console.log(weather);
+    error.style.display = "none";
+  }
 }
 
 searchBox.addEventListener("keypress", (e) => {
